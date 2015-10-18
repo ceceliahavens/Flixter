cce79d6fa8235900998eafc013a4360633c3c587
ceceliahavens <ceceliahavens@hotmail.com>
2015-10-18 23:10:34 +0000

    Hooking up landing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Flixter::Application.routes.draw do
+  root 'static_pages#index'
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +55,4 @@ Flixter::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
