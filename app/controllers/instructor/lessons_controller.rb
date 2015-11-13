9d422a3497445b0d92e15601912d407a806e1e42
ceceliahavens <ceceliahavens@hotmail.com>
2015-11-13 03:04:05 +0000

    retracing steps to find a typo

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -2,7 +2,7 @@ class Instructor::LessonsController < ApplicationController
 	before_action :authenticate_user!
   before_action :require_authorized_for_current_section
 
-  d  before_action :require_authorized_for_current_section
+ before_action :require_authorized_for_current_section
 
   def new
     @lesson = Lesson.new
