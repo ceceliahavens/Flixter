3affea52d3b0eb7feeedf1ed1ea9bd8c3585b684
Cecelia <ceceliahavens@hotmail.com>
2015-11-17 22:50:14 +0000

    ordering lessons in our database

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,8 @@ class Lesson < ActiveRecord::Base
 	belongs_to :section
 	has_one :course, through: :section
 	mount_uploader :video, VideoUploader
+	
+	include RankedModel
+	ranks :row_order, :with_same => :section_id
+	
 end
